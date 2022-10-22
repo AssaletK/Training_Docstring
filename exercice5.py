0d397d0b5c8c2ad3192af52b8dbbac703c03c036
Assaletk <assaletkoua@icloud.com>
2022-10-22 10:16:14 +0000

    Verification mot de passe

--- exercice5.py
+++ exercice5.py
@@ -4,19 +4,19 @@
 #SI le mot de passe ne contient que des nombres AFFICHER "Votre mot de passe ne contient que des nombres."
 #AFFICHER la phrase "Inscription terminée." si le mot de passe est valide.
 
+from curses.ascii import isdigit
+
+
 mdp = input("Entrez un mot de passe (min 8 caractères) : ")
 mdp_trop_court = "votre mot de passe est trop court."
 
-if len(mdp) == 0:
-    print(mdp_trop_court.upper())
-elif len(mdp) < 8:
-    mdp_trop_court = mdp_trop_court.replace("votre", "Votre")
-    if mdp.isdigit() is True:
-        print("Votre mot de passe ne contient que des nombres.")
-    else:
-        print(mdp_trop_court)
+if mdp.isdigit() is True:
+    print("Votre mot de passe ne contient que des nombres.")
 else:
-   if mdp.isdigit() is True:
-       print("Votre mot de passe ne contient que des nombres.")
-   else:
-       print("Inscription terminée.")
+    if len(mdp) == 0
+        print(mdp_trop_court.upper())
+    elif len(mdp) < 8:
+        mdp_trop_court = mdp_trop_court.replace("votre", "Votre")
+        print(mdp_trop_court)
+    else:
+        print("Inscription terminée.")
