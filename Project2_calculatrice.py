7ef76b2d2bd374af7b4f9a54c87d09c1cb99eb91
Assaletk <assaletkoua@icloud.com>
2022-10-23 14:44:42 +0000

    Projet Calculatrice - Gestion d'erreurs

---
+++ Project2_calculatrice.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+#Demander un premier nombre a l'utilisateur
+#Demander un deuxieme nombre a l'utilisateur
+#Additionner ces deux nombres
+#Afficher le resultat
+
+tentative = 0
+
+while tentative >= 0:
+
+    nombre1 = input("Entrer un premier nombre: ")
+    nombre2 = input("Entrer un deuxieme nombre: ")
+
+    if nombre1.isdigit() and nombre2.isdigit():
+        nombre1 = int(nombre1)
+        nombre2 = int(nombre2)
+        print(f"le resultat de l'addition de {nombre1} avec {nombre2} est égal à {nombre1 + nombre2}")
+        break
+    else:
+        print("Veuillez entrer deux nombres valides")
+    tentative += 1
